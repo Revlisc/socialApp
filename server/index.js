70aeab132b21d2c34a3b11ce8b683b33a55e89e9
dales <dalesimmons0210@gmail.com>
2021-10-21 21:44:28 -0700

    initial

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import  mongoose from 'mongoose'
+import cors from 'cors'
+
+import //routes from './routes/routeName.js
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+//import the different routes
+app.use('/route', routes)
+
+const CONNECTION = process.env.CONNECTION
+const PORT = process.env.PORT
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`))
