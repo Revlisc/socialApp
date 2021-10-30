f5ac2e87e32cda153a4d601d1a807f4925436af5
Dale <dalesimmons0210@gmail.com>
2021-10-30 10:23:17 -0700

    structure for oauth

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,1 +1,6 @@
-//need to combine reducers here
+import { combineReducers } from 'redux';
+
+import posts from './posts';
+import auth from './auth'
+
+export const reducers = combineReducers({ posts, auth });
