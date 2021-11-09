fbabec42b5c5b022be06ca0e6e7eb2f06570c274
dales <dalesimmons0210@gmail.com>
2021-11-08 22:16:03 -0800

    setup, fixed routing

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
 import  mongoose from 'mongoose'
 import cors from 'cors'
 
@@ -7,9 +7,12 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+
+dotenv.config()
 
 //import the different routes
 app.use('/posts', postRoutes)
@@ -17,6 +20,6 @@ app.use('/posts', postRoutes)
 const CONNECTION = process.env.CONNECTION
 const PORT = process.env.PORT
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`))
